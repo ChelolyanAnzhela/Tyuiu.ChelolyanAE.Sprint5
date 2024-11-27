c04823185058c07c4713c31c39f346a12c6b9feb
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-11-27 17:42:00 +0500

    Task1

---
+++ Tyuiu.ChelolyanAE.Sprint5.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ChelolyanAE.Sprint5.Task1.V17.Lib
+{
+    public class DataService : ISprint5Task1V17
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExsist=fileInfo.Exists;
+            if (fileExsist)
+            {
+                File.Delete(path);
+            }
+            double y;
+            string str;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                y = Math.Round(2 * i - 4 + ((2 * i - 1) / (Math.Sin(i) + 1)), 2);
+                str = Convert.ToString(y);
+                if (i != stopValue)
+                {
+                    File.AppendAllText(path, str + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+            }
+            return (path);
+        }
+    }
+}
---
+++ Tyuiu.ChelolyanAE.Sprint5.Task1.V17/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ChelolyanAE.Sprint5.Task1.V17.Lib;
+namespace Tyuiu.ChelolyanAE.Sprint5.Task1.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция на диапазноне [-5;5]с шагом 1.                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int a = -5;
+            int b = 5;
+            Console.WriteLine($"Старт шага{a}");
+            Console.WriteLine($"конец шага{b}");
+            string res =ds.SaveToFileTextData(a,b);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Файл {res}");
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
