43ba19a11dd18d67747e9e3d68b8ff65262cdb73
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-12-01 18:30:35 +0500

    Task3

---
+++ Tyuiu.ChelolyanAE.Sprint5.Task3.V7/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.ChelolyanAE.Sprint5.Task3.V7.Lib;
+namespace Tyuiu.ChelolyanAE.Sprint5.Task3.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Потоковый метод записи данных в бинарный файл         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение , вычислить его значение при x = 2, сохранить в файл     *");
+            Console.WriteLine("* и вывести на консоль                                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 2;
+            Console.WriteLine($"x= {x}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine($"Файл: {res}");
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
