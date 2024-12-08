567979130afd3fd64a1aa042170ffe0e9cc7e716
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-12-08 20:26:35 +0500

    Task7

--- Tyuiu.ChelolyanAE.Sprint5.Task7.V20.Lib/DataService.cs
+++ Tyuiu.ChelolyanAE.Sprint5.Task7.V20.Lib/DataService.cs
@@ -1,36 +1,46 @@
 ﻿using System.IO;
 using tyuiu.cources.programming.interfaces.Sprint5;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Tyuiu.ChelolyanAE.Sprint5.Task7.V20.Lib
 {
     public class DataService : ISprint5Task7V20
     {
         public string LoadDataAndSave(string path)
         {
-            string s="";
-
-            string data = File.ReadAllText(path);
-            bool LastChar = false;
-            foreach (char chr in data)
+            string Savepath = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V20.txt");
+            FileInfo fileInfo = new FileInfo(Savepath);
+            if (fileInfo.Exists)
             {
+                File.Delete(Savepath);
+            }
+            string str = File.ReadAllText(path);
+            
 
-                if (chr == 'с')
+            bool LastChar = false;
+            string s = "";
+            foreach (char chr in str)
+            {
+                if ((chr == 'с') || (chr == 'С'))
                 {
-                    if (LastChar)
+                    if (LastChar == true)
                     {
-                        s += 'с';
                         LastChar = false;
                     }
                     else
                     {
                         LastChar = true;
+                        s += 'с';
                     }
                 }
                 else
                 {
-                    LastChar = false; 
-                    s+= chr;
+                    LastChar = false;
+                    s += chr;
                 }
             }
+            File.AppendAllText(Savepath, s);
+            return Savepath;
         }
+        
     }
 }
--- Tyuiu.ChelolyanAE.Sprint5.Task7.V20/Program.cs
+++ Tyuiu.ChelolyanAE.Sprint5.Task7.V20/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Tyuiu.ChelolyanAE.Sprint5.Task7.V20
+﻿using Tyuiu.ChelolyanAE.Sprint5.Task7.V20.Lib;
+namespace Tyuiu.ChelolyanAE.Sprint5.Task7.V20
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу,которая заменяет все удвоенные буквы сс на с         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask7V20.txt";
+            string Savepath = "";
+            Console.WriteLine($"Данные находятся в файле {path}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Savepath = ds.LoadDataAndSave(path);
+            Console.WriteLine($"Файл создан{Savepath}");
+            Console.ReadKey();
         }
     }
 }
